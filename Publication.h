f808be7f2b5ff05c81460dc6f7fd21b1d6a41537
Aryan Khurana <101019909+AryanK1511@users.noreply.github.com>
2023-08-24 11:50:55 -0400

    Update Publication.h

--- Publication.h
+++ Publication.h
@@ -1,15 +1,3 @@
-/*
-*****************************************************************************
-                              Publication.h
-Full Name  : Aryan Khurana
-Student ID#: 145282216
-Email      : akhurana22@myseneca.ca
-Date of completion    : 20 July 2022
-
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*****************************************************************************
-*/
-
 #ifndef SDDS_PUBLICATION_H
 #define SDDS_PUBLICATION_H
 

4a239dcf7db82eeb85b0edb0f8a84047c2ac9907
Aryan Khurana <101019909+AryanK1511@users.noreply.github.com>
2023-08-24 11:48:16 -0400

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,15 +1,3 @@
-/*
-*****************************************************************************
-                              Book.h
-Full Name  : Aryan Khurana
-Student ID#: 145282216
-Email      : akhurana22@myseneca.ca
-Date of completion    : 20 July 2022
-
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*****************************************************************************
-*/
-
 #ifndef SDDS_BOOK_H
 #define SDDS_BOOK_H
 

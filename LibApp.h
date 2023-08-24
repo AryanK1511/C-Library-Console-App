a679190a6dc869b4712db412700f3db9a47251aa
Aryan Khurana <101019909+AryanK1511@users.noreply.github.com>
2023-08-24 11:49:56 -0400

    Update LibApp.h

--- LibApp.h
+++ LibApp.h
@@ -1,15 +1,3 @@
-/*
-*****************************************************************************
-                              LibApp.h
-Full Name  : Aryan Khurana
-Student ID#: 145282216
-Email      : akhurana22@myseneca.ca
-Date of completion    : 05 August 2022
-
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*****************************************************************************
-*/
-
 #ifndef SDDS_LIBAPP_H
 #define SDDS_LIBAPP_H
 

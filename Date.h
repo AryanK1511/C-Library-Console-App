5f0e4f8dec81e586e9aa074f66e0c1aad1410053
Aryan Khurana <101019909+AryanK1511@users.noreply.github.com>
2023-08-24 11:49:10 -0400

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,12 +1,3 @@
-// Final Project Milestone 1
-// Date Module
-// File  Date.h
-// Version 1.0
-// Author   Fardad Soleimanloo
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-/////////////////////////////////////////////////////////////////
 #ifndef SDDS_DATE_H__
 #define SDDS_DATE_H__
 #include <iostream>

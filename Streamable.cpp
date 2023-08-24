113dbbbd772fefcb1f0b7956edf2aa68fb4cee31
Aryan Khurana <101019909+AryanK1511@users.noreply.github.com>
2023-08-24 11:52:49 -0400

    Update Streamable.cpp

--- Streamable.cpp
+++ Streamable.cpp
@@ -1,15 +1,3 @@
-/*
-*****************************************************************************
-                              Streamable.cpp
-Full Name  : Aryan Khurana
-Student ID#: 145282216
-Email      : akhurana22@myseneca.ca
-Date of completion    : 20 July 2022
-
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*****************************************************************************
-*/
-
 #include "Streamable.h"
 
 namespace sdds {
